a3156e99fe5fa93d3f1f6f6a7f571a3fa900538c
unknown <ipark308@gmail.com>
2023-02-26 21:53:14 +0900

    add: 3-3 Todo.go GetTodo

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
-import "golang_toDoList/app/models"
+import (
+	"fmt"
+	"golang_toDoList/app/models"
+)
 
 func main() {
 
-	user := &models.User{}
-	user.Name = "test"
-	user.Email = "test@gmail.com"
-	user.PassWord = "testtest"
-	user.CreateUser()
+	// user := &models.User{}
+	// user.Name = "test"
+	// user.Email = "test@gmail.com"
+	// user.PassWord = "testtest"
+	// user.CreateUser()
 
-	user2, _ := models.GetUser(2)
-	user2.CreateTodo("first todo")
+	// user2, _ := models.GetUser(2)
+	// user2.CreateTodo("first todo")
+
+	t, _ := models.GetTodo(1)
+	fmt.Println(t)
 }
