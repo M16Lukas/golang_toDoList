711e9e1684a422dddda4e620a6315621e7eb3767
unknown <ipark308@gmail.com>
2023-02-26 21:59:58 +0900

    add: 3-4 Todo.go GetTodos

--- main.go
+++ main.go
@@ -13,9 +13,14 @@ func main() {
 	// user.PassWord = "testtest"
 	// user.CreateUser()
 
-	// user2, _ := models.GetUser(2)
-	// user2.CreateTodo("first todo")
+	user2, _ := models.GetUser(2)
+	user2.CreateTodo("Second todo")
 
-	t, _ := models.GetTodo(1)
-	fmt.Println(t)
+	// t, _ := models.GetTodo(1)
+	// fmt.Println(t)
+
+	ts, _ := models.GetTodos()
+	for _, v := range ts {
+		fmt.Println(v)
+	}
 }
