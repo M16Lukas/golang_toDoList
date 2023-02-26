63fccc93535e2ff4a0ab6603f1f354b3f29265f2
unknown <ipark308@gmail.com>
2023-02-26 21:48:35 +0900

    add: Todo.go CreateTodo

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import "golang_toDoList/app/models"
+
 func main() {
 
+	user := &models.User{}
+	user.Name = "test"
+	user.Email = "test@gmail.com"
+	user.PassWord = "testtest"
+	user.CreateUser()
+
+	user2, _ := models.GetUser(2)
+	user2.CreateTodo("first todo")
 }
