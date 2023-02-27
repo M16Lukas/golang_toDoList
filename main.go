570a9e3b0d9b9fa99cdf11bdfa9e4bcc4ca31f17
unknown <ipark308@gmail.com>
2023-02-27 21:28:18 +0900

    add:  3-5 Todo.go GetTodosByUser

--- main.go
+++ main.go
@@ -7,19 +7,19 @@ import (
 
 func main() {
 
-	// user := &models.User{}
-	// user.Name = "test"
-	// user.Email = "test@gmail.com"
-	// user.PassWord = "testtest"
-	// user.CreateUser()
+	user := &models.User{}
+	user.Name = "test2"
+	user.Email = "test2@gmail.com"
+	user.PassWord = "testtest"
+	user.CreateUser()
 
-	user2, _ := models.GetUser(2)
-	user2.CreateTodo("Second todo")
+	user2, _ := models.GetUser(3)
+	user2.CreateTodo("Third todo")
 
 	// t, _ := models.GetTodo(1)
 	// fmt.Println(t)
 
-	ts, _ := models.GetTodos()
+	ts, _ := user2.GetTodosByUser()
 	for _, v := range ts {
 		fmt.Println(v)
 	}
