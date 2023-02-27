5c92d6bc299a621ef2043f0fcba800dbb80b45a1
unknown <ipark308@gmail.com>
2023-02-27 22:06:41 +0900

    add: 4-2 Server JQuery&Bootstrap&StaticFile設定

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -6,6 +6,10 @@ import (
 )
 
 func StartMainServer() error {
+	// css, jsファイルの呼び込み
+	files := http.FileServer(http.Dir(config.Config.Static))
+	http.Handle("/static/", http.StripPrefix("/static/", files))
+
 	// URL登録
 	http.HandleFunc("/", top)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
